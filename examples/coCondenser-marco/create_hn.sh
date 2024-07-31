11fc4d75d1312a00d18695f989b2180f792d275e
Bo van den Berg <bvdb98@gmail.com>
2024-07-31 23:54:54 +0200

    Fix data preparation

--- examples/coCondenser-marco/create_hn.sh
+++ examples/coCondenser-marco/create_hn.sh
@@ -1,6 +1,16 @@
 SCRIPT_DIR=$PWD
+TOKENIZER=bert-base-uncased
+TOKENIZER_ID=bert
+HN_DIR=bert/train-hn
+
 cd marco
-python $SCRIPT_DIR/build_hn.py --tokenizer_name bert-base-uncased --hn_file ../train.rank.txt --qrels qrels.train.tsv \
-  --queries train.query.txt --collection corpus.tsv --save_to bert/train-hn
-ln -s bert/train/* bert/train-hn
+mkdir -p $HN_DIR
+python $SCRIPT_DIR/build_train_hn.py \
+  --tokenizer_name $TOKENIZER \
+  --hn_file ../train.rank.txt \
+  --qrels qrels.train.tsv \
+  --queries train.query.txt \
+  --collection corpus.tsv \
+  --save_to $HN_DIR
+ln -s $TOKENIZER_ID/train/* $HN_DIR
 cd -
