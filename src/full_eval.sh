9df6fd014916f8b2c0a39bbbcf2ff727b959f8e6
Bo van den Berg <bvdb98@gmail.com>
2024-09-02 21:50:42 +0200

    Fix full_eval.sh

--- src/full_eval.sh
+++ src/full_eval.sh
@@ -12,12 +12,12 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 MODEL=$1
-OUT_FOLD=/tmp/"$RANDOM"
+OUT_FOLD=../outputs/full_eval/$(date +%Y-%m-%d-%H:%M:%S)
 mkdir -p "$OUT_FOLD"
 echo $MODEL > "$OUT_FOLD"/model.txt
 
 echo "Encoding queries-----------"
-python -m tevatron.driver.encode \
+srun python -m tevatron.driver.encode \
       --output_dir /tmp/encode \
       --model_name_or_path $MODEL \
       --fp16 \
@@ -32,7 +32,7 @@ TOKENIZER_ID=bert
 for i in $(seq -f "%02g" 0 9)
 do
   echo "Encoding split $i"
-  python -m tevatron.driver.encode \
+  srun python -m tevatron.driver.encode \
         --output_dir /tmp/encode \
         --model_name_or_path $MODEL \
         --fp16 \
@@ -43,7 +43,7 @@ done
 if [ $? -ne 0 ]; then echo "error encoding passages"; rm -r "$OUT_FOLD"; exit 1; fi
 
 echo "Finished encoding passages, now running the retriever-----------"
-python -m tevatron.faiss_retriever \
+srun python -m tevatron.faiss_retriever \
       --query_reps "$OUT_FOLD"/encoded_queries.pt \
       --passage_reps "$OUT_FOLD"/'split*.pt' \
       --depth 1000 \
@@ -53,4 +53,4 @@ python -m tevatron.faiss_retriever \
 if [ $? -ne 0 ]; then echo "error faiss retrieve"; rm -r "$OUT_FOLD"; exit 1; fi
 
 echo "Creating dev rank file"
-python ../examples/coCondenser-marco/score_to_marco "$OUT_FOLD"/dev.rank.tsv
+srun python ../examples/coCondenser-marco/score_to_marco.py "$OUT_FOLD"/dev.rank.tsv
